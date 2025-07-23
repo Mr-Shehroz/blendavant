842eaf28c553d1f4f0738baf24cc9f5b473d23f2
Mr-Shehroz <shehrozkhan.wow@gmail.com>
2025-07-23 13:21:28 +0500

    add

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
                         {/* Search Icon */}
                         <div className="hover:text-gray-300 transition duration-200 cursor-pointer hover:scale-110">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                strokeWidth="1.5" stroke="currentColor" className="2xl:h-[30px] 2xl:w-[30px] xl:w-[22px] xl:h-[22px] lg:w-[18px] lg:h-[18px] w-[13px] h-[13px]">
+                                strokeWidth="1.5" stroke="white" className="2xl:h-[30px] 2xl:w-[30px] xl:w-[22px] xl:h-[22px] lg:w-[18px] lg:h-[18px] w-[13px] h-[13px]">
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                     d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
@@ -53,7 +53,7 @@ export default function Header() {
                         {/* Profile Icon */}
                         <div className="hover:text-gray-300 transition duration-200 cursor-pointer hover:scale-110">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                strokeWidth="1.5" stroke="currentColor" className="2xl:h-[30px] 2xl:w-[30px] xl:w-[22px] xl:h-[22px] lg:w-[18px] lg:h-[18px] w-[13px] h-[13px]">
+                                strokeWidth="1.5" stroke="white" className="2xl:h-[30px] 2xl:w-[30px] xl:w-[22px] xl:h-[22px] lg:w-[18px] lg:h-[18px] w-[13px] h-[13px]">
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                     d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                             </svg>
